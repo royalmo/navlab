20985998bb454863a17097f4b9b71d2a0c5298f1
JoelSantana7 <joelinsquercus@gmail.com>
2023-04-22 18:52:41 +0200

    Added interaction between user list page and database.

--- app/views/users.py
+++ app/views/users.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, url_for, redirect, request
-from flask_login import login_user, login_required, logout_user
+from flask_login import login_user, login_required, logout_user, current_user #############
 
 from ..extensions import db, bcrypt
 from ..models import User, LoginForm, RegisterForm
@@ -38,3 +38,25 @@ def register():
         return render_template('pages/users/signup_success.html.j2', name=form.name.data)
 
     return render_template('pages/users/signup.html.j2', form=form, email_taken=request.method=='POST')
+
+@app.route('/userslist', methods=['GET'])
+@login_required
+def userslist():
+    if current_user.admin:
+        users = User.query.all()
+        #users_time=[]
+        #for user in users:
+            #last_seen = datetime.now()
+            #users_time.append([user,last_seen.strftime('%Y-%m-%d %H:%M:%S.%f')[:-10]])
+        return render_template('pages/userlist.html.j2', userlist=users)
+
+    return render_template('pages/userlist.html.j2', not_admin=True)
+
+@app.route('/userslist/delete/<int:id>')
+@login_required
+def delete(id):
+    if current_user.admin:
+        user = User.query.get_or_404(id)
+        db.session.delete(user)
+        db.session.commit()
+    return redirect(url_for('.userslist'))
