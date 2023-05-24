e9897d9d186198b97288d8aa836095879ba54101
royalmo <royalmo6@gmail.com>
2023-05-24 22:11:07 +0200

    Fixed URL for data_collector

--- api/data_collector.py
+++ api/data_collector.py
@@ -1,44 +1,43 @@
 import requests,time,sys,os,datetime
 
 def main():
-    Site='https://navlab.ericroy.net'
-    Led=Site+'/led'
-    Pot=Site+'/potenciometre'
+    serial_site='http://serial.navlab.ericroy.net'
+    db_site='http://db.navlab.ericroy.net'
     while(True):
         try:
-            r = requests.get(Led)
+            r = requests.get(f"{serial_site}/led")
         except:
-            print('Failed to establish a new connection with',Site)
+            print('Failed to establish a new connection with',serial_site)
         else:
             if r.status_code==200:
                 try:
                     d=r.json()
                     d['time']=str(datetime.datetime.now())
-                    p = requests.post(Led,json=d)
+                    p = requests.post(f"{db_site}/led",json=d)
                 except:
-                    print('Failed to establish a new connection with',Site)
+                    print('Failed to establish a new connection with',db_site)
                 else:
                     if p.status_code!=204:
-                        print('Bad response for POST to',Led,'| Status code:',p.status_code)
+                        print('Bad response for POST to',db_site,'| Status code:',p.status_code)
             else:
-                print('Bad response for GET to',Led,'| Status code:',r.status_code)
+                print('Bad response for GET to',serial_site,'| Status code:',r.status_code)
         try:
-            r = requests.get(Pot)
+            r = requests.get(f"{serial_site}/potenciometre")
         except:
-            print('Failed to establish a new connection with',Site)
+            print('Failed to establish a new connection with',serial_site)
         else:
             if r.status_code==200:
                 try:
                     d=r.json()
                     d['time']=str(datetime.datetime.now())
-                    p = requests.post(Pot,json=d)
+                    p = requests.post(f"{db_site}/potenciometre",json=d)
                 except:
-                    print('Failed to establish a new connection with',Site)
+                    print('Failed to establish a new connection with',db_site)
                 else:
                     if p.status_code!=204:
-                        print('Bad response for POST to',Pot,'| Status code:',p.status_code)
+                        print('Bad response for POST to',db_site,'| Status code:',p.status_code)
             else:
-                print('Bad response for GET to',Pot,'| Status code:',r.status_code)
+                print('Bad response for GET to',serial_site,'| Status code:',r.status_code)
         time.sleep(5)
     
 
